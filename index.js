a4ce4d2a9dbd913dc48df5f672361aa159f6df9a
David Hirtle <david.hirtle@pulseenergy.com>
2013-07-15 12:00:16 -0700

    you must provide a config.json for auth stuff; extract githubapi

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var port = process.env.PORT || 3000;
 var app = require('./app');
 
 app.listen(port, function() {
-	console.log("Listening on " + port);
+	console.log(new Date() + ": listening on " + port);
 });
 module.exports = app;
