46b3a49bfc2e14116ea750e842c59ac676d9f4e8
Neil Gentleman <neil@pulseenergy.com>
2013-07-15 11:39:55 -0700

    forward / to /index.html

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var actions = require('./lib/actions');
 
 
 var app = express();
-app.get('/', function (req, res) {
-	res.send(200);
+app.get('/', function (req, res, next) {
+	req.url = '/index.html';
+	next();
 });
 app.get('/pushes', actions.getOrgCommits);
 app.get('/diff/:repo/:sha', actions.getCommitDiff);
