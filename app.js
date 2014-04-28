c8775bce02b05c115103f24585706d0ae07664f7
Neil Gentleman <ngentleman@gmail.com>
2014-04-28 13:50:23 -0700

    extract auth to separate files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var url = require('url');
-var _ = require('underscore');
+var request = require('request');
+var githubApi = require('./lib/githubApi');
 var actions = require('./lib/actions');
 
 var app = express();
@@ -12,80 +12,14 @@ app.use(express.cookieParser());
 app.use(express.session({ secret: 'f8fb234f3cf333241e3f7c74' }));
 
 if (process.env.GITHUB_API_TOKEN) {
-	var request = require('request');
-	var githubApi = require('./lib/githubApi');
-
-	function settable() {
-		var value;
-		var waiting = [];
-
-		return {
-			get: function (cb) {
-				if (waiting) {
-					waiting.push(cb);
-				} else {
-					cb(value);
-				}
-			},
-			set: function (v) {
-				waiting.forEach(function (cb) {
-					cb(v);
-				});
-				value = v;
-				waiting = null;
-			}
-		};
-	}
-
 	var token = process.env.GITHUB_API_TOKEN;
-	var user = settable();
 
-	request(githubApi.user(token), function (err, response, body) {
-		user.set(JSON.parse(body));
-	});
-
-	app.use(function (req, res, next) {
-		user.get(function (user) {
-			req.user = user;
-			req.session.authToken = token;
-
-			if (res.locals) {
-				res.locals.everyauth = {
-					loggedIn: !!req.user
-				};
-				res.locals.user = req.user;
-			}
-			next();
-		});
-	});
+	app.use(require('./lib/noauth')(token));
 } else {
-	var everyauth = require('everyauth');
+	var id = process.env.GITHUB_APP_ID;
+	var secret = process.env.GITHUB_APP_SECRET;
 
-	everyauth.everymodule.findUserById(function (req, id, callback) {
-		callback(null, req.session.auth.github.user);
-	});
-
-	everyauth.debug = true;
-
-	everyauth.github
-		.scope('repo') // require read access to private repos
-		.appId(process.env.GITHUB_APP_ID)
-		.appSecret(process.env.GITHUB_APP_SECRET)
-		.entryPath('/auth/github')
-		.callbackPath('/auth/github/callback')
-		.findOrCreateUser( function (session, accessToken, accessTokenExtra, githubUserMetadata) {
-			session.authToken = accessToken; // shortcut for session.auth.github.accessToken
-			return githubUserMetadata;
-		})
-		.handleAuthCallbackError( function (req, res) {
-			// override to avoid jade dependency in github module
-			var parsedUrl = url.parse(req.url, true);
-			res.render('authfail', {
-				errorDescription: _.compact([parsedUrl.query.error, parsedUrl.query.error_description]).join('; ')
-			});
-		})
-		.redirectPath('/');
-	app.use(everyauth.middleware());
+	app.use(require('./lib/everyauth')(id, secret));
 }
 
 app.use(app.router);
