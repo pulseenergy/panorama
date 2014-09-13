6ed03167e8590e262485694e24b5bd113eecd0ed
Neil Gentleman <ngentleman@gmail.com>
2014-09-13 00:21:56 -0700

    use node-replay to have consistent data when developing

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var port = process.env.PORT || 3456;
+if (process.env.REPLAY) {
+	require('replay');
+}
 require('./app').listen(port, function() {
 	console.log(new Date() + ": listening on " + port);
 });
